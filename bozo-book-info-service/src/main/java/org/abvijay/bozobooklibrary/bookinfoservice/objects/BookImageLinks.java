f2f4c2a1939406bf46b0aaf059ec6d31801c0507
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-05-31 15:17:17 +0530

    added more fields

--- bozo-book-info-service/src/main/java/org/abvijay/bozobooklibrary/bookinfoservice/objects/BookImageLinks.java
+++ bozo-book-info-service/src/main/java/org/abvijay/bozobooklibrary/bookinfoservice/objects/BookImageLinks.java
@@ -3,6 +3,42 @@ package org.abvijay.bozobooklibrary.bookinfoservice.objects;
 public class BookImageLinks {
     String smallThumbnail;
     String thumbnail;
+    String small;
+    String medium;
+    String large;
+    String extraLarge;
+
+    public String getSmall() {
+        return this.small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+    public String getMedium() {
+        return this.medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getLarge() {
+        return this.large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
+
+    public String getExtraLarge() {
+        return this.extraLarge;
+    }
+
+    public void setExtraLarge(String extraLarge) {
+        this.extraLarge = extraLarge;
+    }    
 
     public String getSmallThumbnail() {
         return this.smallThumbnail;
